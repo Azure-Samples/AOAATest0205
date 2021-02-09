01f0daea6ca906a942295604d056754d8e616e94
Bin Li <binli@ntdev.microsoft.com>
2021-02-09 11:28:58 -0800

    Change from 0209

--- TestSolution0205/ConsoleApp1/Program.cs
+++ TestSolution0205/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World from Feature0205!");
+            Console.WriteLine("Hello World from Feature0209!");
         }
     }
 }
